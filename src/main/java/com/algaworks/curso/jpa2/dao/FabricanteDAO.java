5e2ebeeeeaed6ad5270f155b664e6261d7c7a294
Willian Alves da Silva <willian_alves_df@hotmail.com>
2020-06-11 16:15:59 -0300

    netbeans

---
+++ src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.algaworks.curso.jpa2.dao;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Willian
+ */
+public class FabricanteDAO implements Serializable{
+    
+    
+}
---
+++ src/main/java/com/algaworks/curso/jpa2/models/Fabricante.java
@@ -0,0 +1,30 @@
+package com.algaworks.curso.jpa2.models;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Willian
+ */
+@Entity
+public class Fabricante implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+    
+}
